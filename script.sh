09c0b0829b09fe4a0557e006963a2435b5be7c5a
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-03-03 10:39:05 +0300

    Updated travis.iml. Travis, come on!

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
+
 ./gradlew clean build connectedAndroidTest
