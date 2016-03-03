cb5ee9c96ee865865752c47991221e5bbd32515e
Savelii Zagurskii <s.zagurskii@jeymoney.me>
2016-03-03 10:29:35 +0300

    Deleted unused files. Updated travis.iml.

---
+++ script.sh
@@ -0,0 +1,2 @@
+# This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
+./gradlew clean build connectedAndroidTest
